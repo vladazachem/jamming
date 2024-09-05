980398eb09787ad59a2094dc89b6c2bf32db3641
vladazachem <71698265+vladazachem@users.noreply.github.com>
2024-09-05 13:44:42 +0200

    SearchBar ui complited

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,12 +4,14 @@ import styles from './Home.module.css';
 import '../../style.css';
 
 import Header from "../Header/Header";
+import SearchBar from "../SearchBar/SearchBar";
 
 
 const Home = () => {
   return (
     <div className={styles.main}>
       <Header />
+      <SearchBar />
     </div>
   );
 };
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,3 +2,27 @@ import React from 'react';
 
 import styles from './SearchBar.module.css';
 import '../../style.css';
+
+import search_btn from './search.svg';
+
+const SearchBar = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.wrap}>
+                <form className={styles.searchInput}>
+                    <button className={styles.search_btn}>
+                        <img src={search_btn} alt="Search"/>                        
+                    </button>
+                    <input className={`input ${styles.input}`} type="search" aria-label="searchinput" placeholder="What do you want to find?" />
+                </form>
+                <div className={styles.btns_section}>
+                    <button className={`S_button ${styles.S_button}`}>All</button>
+                    <button className={`S_button ${styles.S_button}`}>Artist</button>
+                    <button className={`S_button ${styles.S_button}`}>Title</button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default SearchBar;
